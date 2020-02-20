f1818f61e7d20b3326121e2e969e86ba656d80ca
JOULKVA Johan <johan.akyos@gmail.com>
2020-02-20 17:17:10 +0100

    Command app:bdd-dump + app:bdd-import

---
+++ Command/bddImport.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Akyos\CoreBundle\Command;
+
+use Doctrine\DBAL\Connection;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class bddImport extends Command
+{
+
+    protected static $defaultName = 'app:bdd-import';
+
+    private $connection;
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('')
+            ->setHelp('');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $finder = new Finder();
+        $finder->files()->in(__DIR__ . '/../../../src/Dump/');
+        $finder->sortByAccessedTime()->reverseSorting();
+
+        foreach ($finder as $file) {
+            $process = new Process('mysql -u '.$this->connection->getParams()['user'].' -p '.$this->connection->getParams()['dbname'].' < '.$file->getPathname());
+            break;
+        }
+        $process->run();
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        $output->writeln([
+            $process->getOutput()
+        ]);
+
+        return 0;
+    }
+
+}
