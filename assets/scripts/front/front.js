3c81dfff6b8acbf391e6cf2edb8c8e87855cfbb5
Thomas Sebert <thomas.sebert.akyos@gmail.com>
2020-03-02 15:30:07 +0100

    Try to add lazyload

--- assets/scripts/front/front.js
+++ assets/scripts/front/front.js
@@ -2,9 +2,11 @@
 import 'jquery';
 
 import Rgpd from "./modules/Rgpd";
+import LazyLoad from "../../../../FileManagerBundle/assets/scripts/modules/lazyLoad";
 
 class Front {
 	static init() {
+		LazyLoad.init();
 		Rgpd.init();
 	}
 }
