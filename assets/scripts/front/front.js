302b11903690996657905a0b9dd4685b1509b2dc
Thomas Sebert <thomas.sebert.akyos@gmail.com>
2020-03-02 15:40:49 +0100

    Try to load new js files...

--- assets/scripts/front/front.js
+++ assets/scripts/front/front.js
@@ -2,12 +2,9 @@
 import 'jquery';
 
 import Rgpd from "./modules/Rgpd";
-import LazyLoad from "../../../../FileManagerBundle/assets/scripts/modules/lazyLoad";
 
 class Front {
 	static init() {
-		LazyLoad.init();
-		console.log('changed');
 		Rgpd.init();
 	}
 }
