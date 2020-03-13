4e5db9388c798860ce7ef6eb68b8f3d60657eb07
Alexis <alexis.bouchard.akyos@gmail.com>
2020-03-13 10:06:31 +0100

    feat: add fixtures to load user admin

---
+++ DataFixtures/UserAdminFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Akyos\CoreBundle\DataFixtures;
+
+use Akyos\CoreBundle\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserAdminFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user
+            ->setEmail("admin@akyos.fr")
+            ->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'root'
+            ))
+            ->setRoles(["ROLE_SUPER_ADMIN"])
+        ;
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
