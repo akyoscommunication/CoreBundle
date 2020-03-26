c49767146f70a4cb807b490fa1fbe2b34968dfa5
Thomas Sébert <tom@Mac-mini-de-Thomas-2.home>
2020-03-26 13:45:41 +0100

    Add createdAt in post form

--- Form/PostType.php
+++ Form/PostType.php
@@ -50,6 +50,7 @@ class PostType extends AbstractType
             ])
             ->add('createdAt', DateType::class, [
                 'widget' => 'single_text',
+                'label' => 'Date de publication'
             ])
         ;
     }
