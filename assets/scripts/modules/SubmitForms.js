0b4cfed4deb21087e0504dfe7a72310d77abc6a6
rudy <rudy.akyos@gmail.com>
2020-10-20 15:17:41 +0200

    crud/element/table if field==File

--- assets/scripts/modules/SubmitForms.js
+++ assets/scripts/modules/SubmitForms.js
@@ -5,7 +5,7 @@ class SubmitForm {
             const forms = [...document.querySelectorAll('form:not(.not-submit)')];
             console.log(forms);
             forms.reduce((previous, next) => previous.then(() => { return that.post(next); }), that.initSubmit())
-                .then(() => {window.location.reload();})
+                .then(() => {})
                 .catch( (err) => {console.log(err);});
         });
     }
