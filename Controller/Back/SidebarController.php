e54ad7d46d2703f76d6b69c04ad28414b87e570f
Thomas SEBERT <thomas.sebert.akyos@gmail.com>
2020-12-28 18:49:08 +0100

    Refactor sidebar controller

--- Controller/Back/SidebarController.php
+++ Controller/Back/SidebarController.php
@@ -3,6 +3,7 @@
 namespace Akyos\CoreBundle\Controller\Back;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Response;
 
 class SidebarController extends AbstractController
@@ -10,29 +11,17 @@ class SidebarController extends AbstractController
     public function getBundleSidebar($route): Response
     {
         $html = "";
-
-        if (class_exists('Akyos\BuilderBundle\AkyosBuilderBundle'))
-        {
-            $response = $this->forward('Akyos\BuilderBundle\Service\ExtendSidebar::getTemplate', ['route' => $route]);
-            $html .= $response->getContent();
-        }
-
-        if (class_exists('Akyos\FormBundle\AkyosFormBundle'))
-        {
-            $response = $this->forward('Akyos\FormBundle\Service\ExtendSidebar::getTemplate', ['route' => $route]);
-            $html .= $response->getContent();
-        }
-        if (class_exists('Akyos\FileManagerBundle\AkyosFileManagerBundle'))
-        {
-            $response = $this->forward('Akyos\FileManagerBundle\Service\ExtendSidebar::getTemplate', ['route' => $route]);
-            $html .= $response->getContent();
-        }
-
-        if (class_exists('Akyos\ShopBundle\AkyosShopBundle'))
-        {
-            $response = $this->forward('Akyos\ShopBundle\Service\ExtendSidebar::getTemplate', ['route' => $route]);
-            $html .= $response->getContent();
-        }
+	
+		$finder = new Finder();
+		$finder->depth('== 0');
+		foreach ($finder->directories()->in($this->getParameter('kernel.project_dir').'/lib') as $bundleDirectory) {
+			if(class_exists('Akyos\\'.$bundleDirectory->getFilename().'\Service\ExtendSidebar')) {
+				if(method_exists('Akyos\\'.$bundleDirectory->getFilename().'\Service\ExtendSidebar', 'getTemplate')) {
+					$response = $this->forward('Akyos\\' . $bundleDirectory->getFilename() . '\Service\ExtendSidebar::getTemplate', ['route' => $route]);
+					$html .= $response->getContent();
+				}
+			}
+		}
 
         return new Response($html);
     }
@@ -53,12 +42,17 @@ class SidebarController extends AbstractController
     public function getOptionsSidebar($route): Response
     {
         $html = "";
-
-        if (class_exists('Akyos\BuilderBundle\Service\ExtendSidebar'))
-        {
-            $response = $this->forward('Akyos\BuilderBundle\Service\ExtendSidebar::getOptionsTemplate', ['route' => $route]);
-            $html .= $response->getContent();
-        }
+	
+		$finder = new Finder();
+		$finder->depth('== 0');
+		foreach ($finder->directories()->in($this->getParameter('kernel.project_dir').'/lib') as $bundleDirectory) {
+			if(class_exists('Akyos\\'.$bundleDirectory->getFilename().'\Service\ExtendSidebar')) {
+				if(method_exists('Akyos\\'.$bundleDirectory->getFilename().'\Service\ExtendSidebar', 'getOptionsTemplate')) {
+					$response = $this->forward('Akyos\\' . $bundleDirectory->getFilename() . '\Service\ExtendSidebar::getOptionsTemplate', ['route' => $route]);
+					$html .= $response->getContent();
+				}
+			}
+		}
 
         return new Response($html);
     }
