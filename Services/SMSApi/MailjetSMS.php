6883ce7264312acbf0583f420427512d09536213
Thomas SEBERT <thomas.sebert.akyos@gmail.com>
2021-05-05 10:09:36 +0200

    Change phone number validation

--- Services/SMSApi/MailjetSMS.php
+++ Services/SMSApi/MailjetSMS.php
@@ -6,26 +6,30 @@ use Akyos\CoreBundle\Services\MessageLogger;
 use Mailjet\Client;
 use Mailjet\Resources;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 
 // Needs composer require mailjet/mailjet-apiv3-php
 class MailjetSMS
 {
-	
 	private $sender;
 	private $smsToken;
 	private $messageLogger;
+	private $flashBag;
 	
-	public function __construct(ParameterBagInterface $params, MessageLogger $messageLogger)
+	public function __construct(ParameterBagInterface $params, MessageLogger $messageLogger, FlashBagInterface $flashBag)
 	{
 		$this->smsToken = $params->get('mailjet_smsToken');
 		$this->sender = $params->get('mailjet_sender');
 		$this->messageLogger = $messageLogger;
+		$this->flashBag = $flashBag;
 	}
 	
 	public function sendSMS(string $phoneNumber, string $body, bool $doNotFlush = null)
 	{
 		$phoneNumber = static::transformNum($phoneNumber);
+		// IF ERROR
 		if (is_array($phoneNumber)) {
+			$this->flashBag->add('danger', 'Le format du numéro de téléphone est invalide, il doit correspondre au format international E.164 (exemple +33612345678 pour la France)');
 			return $phoneNumber;
 		}
 		
@@ -52,23 +56,16 @@ class MailjetSMS
 	
 	public static function transformNum($number)
 	{
-		preg_match_all("/^0([0-9].?){9}/", $number, $matches);
-		if (count($matches[0])) {
-			$number = preg_replace('/0/', '+33', $number, 1);
-			$number = str_replace(".", "", $number);
-			$number = str_replace(" ", "", $number);
-		} else {
-			preg_match_all('/^\+33([0-9].?){9}/', $number, $matches);
-			if (count($matches[0]) == 0) {
-				return [
-					"status" => false,
-					"message" => "Format du numéro de téléphone invalide",
-					"errorcode" => "ERRNUMFORMAT"
-				];
-			}
-			$number = str_replace(".", "", $number);
-			$number = str_replace(" ", "", $number);
+		preg_match_all('/^\+[1-9]\d{1,14}$/', $number, $matches);
+		if (count($matches[0]) === 0) {
+			return [
+				"status" => false,
+				"message" => "Format du numéro de téléphone invalide",
+				"errorcode" => "ERRNUMFORMAT"
+			];
 		}
+		$number = str_replace([".", " "], "", $number);
+		
 		return $number;
 	}
 }
