637ac6a31539833662409b729bed1303cd4ce2e7
Lilian Carneau <lilian.akyos@gmail.com>
2021-07-27 10:14:49 +0200

    fix: add scheme and session puppeteer

--- Services/Puppeteer.php
+++ Services/Puppeteer.php
@@ -3,6 +3,8 @@
 namespace Akyos\CoreBundle\Services;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 
@@ -12,11 +14,13 @@ class Puppeteer
     private $em;
     /** @var KernelInterface */
     private $kernel;
+    private $request;
 
-    public function __construct(EntityManagerInterface $em, KernelInterface $kernel)
+    public function __construct(EntityManagerInterface $em, KernelInterface $kernel, RequestStack $requestStack)
     {
         $this->em = $em;
         $this->kernel = $kernel;
+        $this->request = $requestStack->getCurrentRequest();
     }
 
     public function generatePDF($fileName, $path, $dl = true, $pathOutput = false)
@@ -25,7 +29,7 @@ class Puppeteer
         $output = $this->kernel->getProjectDir().'/documents/'.$fileName;
         $pup = $this->kernel->getProjectDir().'/puppeteer/generate.js';
 
-        shell_exec('node '.$pup.' '.$linkTo.' '.$output);
+        shell_exec('node '.$pup.' '.$linkTo.' '.$output.' '.$this->request->cookies->get('PHPSESSID').' '.$this->request->getSchemeAndHttpHost());
 
         $content = file_get_contents($output);
         if ($dl) {
