ddc7b726f9188313d2e7be958d7ec170cb4508c5
Thomas SEBERT <thomas.sebert.akyos@gmail.com>
2021-11-15 15:22:49 +0100

    Fix is granted use statement

--- Controller/Back/ProfileController.php
+++ Controller/Back/ProfileController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/admin/profile", name="profile_")
