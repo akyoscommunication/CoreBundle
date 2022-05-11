b11686902ee9a3f47574ea036b6f4f6233f2ae47
akyoscommunication <50996280+akyoscommunication@users.noreply.github.com>
2022-05-11 16:13:12 +0200

    Update AkyosCoreBundle.php

--- AkyosCoreBundle.php
+++ AkyosCoreBundle.php
@@ -3,11 +3,12 @@
 namespace Akyos\CoreBundle;
 
 use Akyos\CoreBundle\DependencyInjection\CoreBundleExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AkyosCoreBundle extends Bundle
 {
-	public function getContainerExtension()
+	public function getContainerExtension(): ?ExtensionInterface
 	{
 		if (null === $this->extension) {
 			$this->extension = new CoreBundleExtension();
